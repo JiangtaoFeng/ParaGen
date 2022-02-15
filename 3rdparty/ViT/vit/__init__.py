2700ed1ad68978f3319e7ba33c78d0b295b13be9
JiangtaoFeng <fengjt16@fudan.edu.cn>
2022-02-15 16:34:01 +0800

    add import

--- 3rdparty/ViT/vit/__init__.py
+++ 3rdparty/ViT/vit/__init__.py
@@ -2,3 +2,4 @@ from .image_dataset import ImageDataset
 from .vit_encoder import ImageTransformerEncoder
 from .vit_model import ViTModel
 from .vit_task import ViTTask
+from .cosine_annealing_rate_scheduler import CosineAnnealingRateScheduler
