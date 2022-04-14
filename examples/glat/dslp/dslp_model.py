dfeb4b26cf4ce419b591223950535eca6ba66592
JiangtaoFeng <fjt159875321>
2022-04-14 16:26:36 +0800

    remove unused import

--- examples/glat/dslp/dslp_model.py
+++ examples/glat/dslp/dslp_model.py
@@ -1,10 +1,4 @@
 from paragen.models import register_model
-from paragen.models.abstract_encoder_decoder_model import AbstractEncoderDecoderModel
-from paragen.modules.decoders import create_decoder
-from paragen.modules.encoders import create_encoder
-from paragen.modules.layers.classifier import LinearClassifier
-from paragen.modules.utils import create_source_target_modality, uniform_assignment, create_sequence
-from paragen.utils.ops import local_seed
 
 from examples.glat.glat.glat import GLATModel
 
